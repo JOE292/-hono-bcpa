9dd933872efed95264411d6b49df0f79b2285bb0
JOE292 <yohanes.mesfin@bitscollege.edu.et>
2026-04-29 13:47:05 -0700

    Create client.ts

---
+++ src/db/client.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is missing in .env");
+}
+
+const queryClient = postgres(connectionString);
+export const db = drizzle(queryClient);
